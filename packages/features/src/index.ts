9885e1432b71972396260065ef6cae338e6d409d
milliondreamsblog <114696765+milliondreamsblog@users.noreply.github.com>
2026-06-04 10:44:59 +0530

    feat(features): export cart/featured-product/host-controls

--- packages/features/src/index.ts
+++ packages/features/src/index.ts
@@ -3,3 +3,6 @@ export * from './usePolls';
 export * from './useCoupons';
 export * from './useCheckout';
 export * from './useDeliveryTracking';
+export * from './useCart';
+export * from './useFeaturedProduct';
+export * from './useHostControls';
